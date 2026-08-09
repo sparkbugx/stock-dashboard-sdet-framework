e44e7280caf0b51b13424495f57eb1bae67191fb
Dumpling <dumplingone@proton.me>
2026-08-08 20:46:30 -0600

    New things added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
+// src/app/app.component.ts
+
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('stock-dashboard-sdet-framework');
+export class AppComponent {
+  title = 'calculator';
+  display: string = '';
+
+  appendInput(value: string): void {
+    this.display += value;
+  }
+
+  clear(): void {
+    this.display = '';
+  }
+
+  calculate(): void {
+    try {
+      this.display = eval(this.display);
+    } catch (e) {
+      this.display = 'Error';
+    }
+  }
 }
